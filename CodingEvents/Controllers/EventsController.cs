329252573861624d75bf6ddbe71e1837f5d11bd7
Gretchen Wagner <gretchenwag@gmail.com>
2021-01-26 15:25:55 -0600

    Add project files.

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        static private List<string> Events = new List<string>();
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //Events.Add("Strange Loop");
+            //Events.Add("Grace Hopper");
+            //Events.Add("Code with Pride");
+            ViewBag.events = Events;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/Add")]
+        public IActionResult NewEvent(string name)
+        {
+            Events.Add(name);
+
+            return Redirect("/Events");
+        }
+    }
+}
