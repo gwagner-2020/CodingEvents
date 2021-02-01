82ce724e2ec67d4891c9915946930bbdb78c7ac9
Gretchen Wagner <gretchenwag@gmail.com>
2021-02-01 14:44:48 -0600

    completed chap16 lecture

--- CodingEvents/Models/EventType.cs
+++ CodingEvents/Models/EventType.cs
@@ -7,9 +7,22 @@ namespace CodingEvents.Models
 {
     public enum EventType
     {
-        Conference,
-        Meetup,
-        Workshop,
-        Social
+        Conference, // 0 -- backing value
+        Meetup,     // 1 -- backing value
+        Workshop,   // 2 -- backing value
+        Social      // 3 -- backing value
     }
+
+    /* EventType eType = EventType.Meetup;
+     * Console.WriteLine((int)eType); --> prints "1"
+     * Console.WriteLine(eType); --> prints "Meetup"
+     * 
+     * Console.WriteLine(eType == "Meetup") --> eType not string, so doesn't print
+     * Console.WriteLine(eType == EventType.Meetup) --> prints "true"
+     * 
+     * int option = 3;
+     * EventType optionType = (EventType)option;
+     * Console.WriteLine(optionType); --> prints "Social"
+     */
+
 }
